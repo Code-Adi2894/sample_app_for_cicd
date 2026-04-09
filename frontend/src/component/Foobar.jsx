2e3561080fa3290087e0bcfa6583e1f698f721d3
Code-Adi2894 <rkl.adipatel2894@gmail.com>
2026-04-09 22:28:46 +0530

    changes made

--- frontend/src/component/Foobar.jsx
+++ frontend/src/component/Foobar.jsx
@@ -1,7 +1,5 @@
 const Foobar = () => {
-  return (
-    <div id="foobar">Made with 💙 Vxrcel | {new Date().getFullYear()}</div>
-  );
+  return <div id="foobar">Made with Vxrcel | {new Date().getFullYear()}</div>;
 };
 
 export default Foobar;
