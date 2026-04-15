40e5bda4c10befab43f244d08facb842ba673e63
Code-Adi2894 <rkl.adipatel2894@gmail.com>
2026-04-15 18:00:59 +0530

    changes

--- frontend/src/__tests__/Greetings.test.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Greetings from '../component/Greetings';
-
-test('renders greeting', () => {
-  render(<Greetings />);
-  expect(screen.getByText(/Hello, World!/)).toBeInTheDocument();
-});
--- frontend/src/__tests__/dummy2.test.js
+++
@@ -1,6 +0,0 @@
-import { sum } from "../component/foo";
-
-it("sums number", () => {
-  expect(sum(1, 2)).toEqual(3);
-  expect(sum(2, 2)).toEqual(4);
-});
